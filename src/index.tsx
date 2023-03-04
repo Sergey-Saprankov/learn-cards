8169545767a8226d05b7ac1f3b6d03bae20489c6
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-03-04 21:01:17 +0300

    added BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,21 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './app/App'
 import reportWebVitals from './reportWebVitals'
-import { store } from './store/store'
+
+import { store } from 'store/store'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
